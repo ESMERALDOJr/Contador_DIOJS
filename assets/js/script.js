dea534881f519a6685ecf8876f4146e1d534b632
ESMERALDOJr <esmeraldo.log@hotmail.com>
2022-02-02 15:17:48 -0300

    Alteração na cor do valor quando negativo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,34 @@
-const CURRENT_NUMBER = document.getElementById('currentNumber');
+let CURRENT_NUMBER = document.getElementById('currentNumber');
+const decrement = document.getElementById('decrement')
+const increment = document.getElementById('increment')
 let count = 0;
 
 
-function increment() {
-    count++;
-    if (count <= 10) {
-        CURRENT_NUMBER.innerHTML = count;
-    }
-}
+decrement.addEventListener('click',
+    function decrement() {
+        count--;
+        if (count >= -10) {
+            CURRENT_NUMBER.innerHTML = count;
+        }
 
-
-function decrement() {
-    count--;
-    if (count >= -10) {
-        CURRENT_NUMBER.innerHTML = count;
+        if (count <= 0) {
+            CURRENT_NUMBER.classList.add('redcolor')
+        }
+        
     }
-}
+)
+
+increment.addEventListener('click',
+    function increment() {
+        count++;
+        if (count <= 10) {
+            CURRENT_NUMBER.innerHTML = count;
+        }
+        if (count >= 0) {
+            CURRENT_NUMBER.classList.remove('redcolor')
+        }         
+
+       
+       
+    }
+)
